a32a47868368e3d7d635ac070fbc5fa6a0c78fa0
Matt Ramotar <matt.ramotar@jhu.edu>
2021-03-27 19:18:12 -0400

    Refactor init

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRoutes, Navigate } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/core';
-import { PayPalScriptProvider, PayPalButtons } from '@paypal/react-paypal-js';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 
-import { paypal } from './env';
 import routes from './routes';
-import { generateRoutes } from './lib';
+import { generateRoutes } from './lib/generators';
+import { getPayPalClientId } from './lib/getters';
 import theme from './theme';
 
 import '../src/sass/App.scss';
 
-function App() {
+interface Options {
+  'client-id': string;
+  currency: string;
+  intent: string;
+}
+
+function App(): JSX.Element | null {
+  const [clientId, setClientId] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
   const routing = useRoutes([
     ...generateRoutes(routes),
     {
@@ -19,16 +28,37 @@ function App() {
     },
   ]);
 
-  const initialOptions = {
-    'client-id':
-      process.env.REACT_APP_PAYPAL_CLIENT_ID || paypal.PAYPAL_CLIENT_ID,
-    currency: 'USD',
-    intent: 'capture',
+  useEffect(() => {
+    const init = async (): Promise<void> => {
+      const slug = process.env.REACT_APP_PAYPAL_CLIENT_ID_SLUG || '_';
+      const clientId = await getPayPalClientId(slug);
+      setClientId(clientId);
+    };
+
+    init();
+  });
+
+  useEffect(() => {
+    if (clientId) {
+      setIsLoading(false);
+    }
+  }, [clientId]);
+
+  const buildOptions = (): Options => {
+    const options = {
+      'client-id': clientId,
+      currency: 'USD',
+      intent: 'capture',
+    };
+
+    return options;
   };
+
+  if (isLoading) return null;
 
   return (
     <ThemeProvider theme={theme}>
-      <PayPalScriptProvider options={initialOptions}>
+      <PayPalScriptProvider options={buildOptions()}>
         {routing}
       </PayPalScriptProvider>
     </ThemeProvider>
